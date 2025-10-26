37bb2bf2689b159c34af5930c0959ecc5209c11e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-26 11:53:27 +0300

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { signIn } from "../auth";
+import { redirect } from "next/dist/server/api-utils";
 
 export const metadata = {
   title: "Login",
@@ -6,8 +8,16 @@ export const metadata = {
 };
 
 const Login = () => {
+  const LOGIN = async (formData) => {
+    "use server";
+    // const email = formData.get("email");
+    // const password = formData.get("password");
+    // console.log("Email:", email);
+    // console.log("Password:", password);
+    await signIn("google", { redirect: "/" });
+  };
   return (
-    <form className="max-w-sm mx-auto">
+    <form action={LOGIN} className="max-w-sm mx-auto">
       <div className="mb-5">
         <label
           htmlFor="email"
@@ -18,6 +28,7 @@ const Login = () => {
         <input
           type="email"
           id="email"
+          name="email"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="name@flowbite.com"
           required
@@ -26,6 +37,7 @@ const Login = () => {
       <div className="mb-5">
         <label
           htmlFor="password"
+          name="password"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
           Your password
@@ -60,6 +72,31 @@ const Login = () => {
       >
         Submit
       </button>
+      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+        Don’t have an account yet?{" "}
+        <a
+          href="#"
+          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+        >
+          Sign up
+        </a>
+      </p>
+      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+        <a
+          href="#"
+          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+        >
+          Forgot password?
+        </a>
+      </p>
+      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+        <button
+          onClick={LOGIN}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Login With Google
+        </button>
+      </p>
     </form>
   );
 };
